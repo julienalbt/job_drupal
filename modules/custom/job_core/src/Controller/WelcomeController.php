744747970479dfa34a0ca4a9282fc9217a474197
Julien <alombertjulien@gmail.com>
2021-02-21 11:15:50 +0100

    fix issues

---
+++ modules/custom/job_core/src/Controller/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Drupal\job_core\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class WelcomeController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function content() {
+
+    return [];
+  }
+
+}
