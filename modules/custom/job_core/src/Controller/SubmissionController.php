2d5235a9cd3f71b92fd88a37b0f26bf08fb220a3
Julien <alombertjulien@gmail.com>
2021-02-24 13:54:05 +0100

    fixes

---
+++ modules/custom/job_core/src/Controller/SubmissionController.php
@@ -0,0 +1,38 @@
+<?php
+namespace Drupal\job_core\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\webform\Entity\WebformSubmission;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class SubmissionController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @param $sid
+   *   Submission ID.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function delete($sid) {
+    $query = \Drupal::entityQuery('webform_submission')
+      ->condition('webform_id', 'candidature')
+      ->condition('sid', $sid)
+      ->accessCheck(FALSE);
+
+    $result = $query->execute();
+    foreach ($result as $item) {
+      $submission = WebformSubmission::load($item);
+      $submission->delete();
+    }
+
+    return new RedirectResponse(Url::fromRoute('view.liste_candidatures_candidat.page_1')->toString(), 301);
+  }
+
+}
